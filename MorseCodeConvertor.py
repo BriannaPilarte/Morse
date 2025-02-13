a8646ba207d9365edf48fadce656c829c1f9564f
BriannaPilarte <143533314+BriannaPilarte@users.noreply.github.com>
2025-02-13 09:34:04 -0500

    Add files via upload

---
+++ MorseCodeConvertor.py
@@ -0,0 +1,19 @@
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+    'Z': '--..', '0': '-----', '1': '.----', '2': '..---',
+    '3': '...--', '4': '....-', '5': '.....', '6': '-....',
+    '7': '--...', '8': '---..', '9': '----.', ' ': '/'
+}
+
+def text_to_morse(text):
+    text = text.upper()  # Convert text to uppercase
+    return ' '.join(MORSE_CODE_DICT.get(char, '') for char in text)
+
+if __name__ == "__main__":
+    line = input("Enter a line of text: ")
+    morse_code = text_to_morse(line)
+    print("Morse Code:", morse_code)
